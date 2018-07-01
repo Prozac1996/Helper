45b0326fcc97c5e0f121580b7580b9940790858e
liufangzheng <liufangzheng@jd.com>
2018-07-01 11:06:37 +0800

    复杂型邮箱版本

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import javafx.embed.swing.JFXPanel;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
@@ -11,16 +9,21 @@ import java.util.ArrayList;
 
 public class Main {
 
+    static JTextField jTextField_1,jTextField_2,jTextField_3,jTextField_6_1,jTextField_6_2,jTextField_6_3;
+    static JComboBox jComboBox;
+
+    static ArrayList<String[]> infos = new ArrayList<>();
+    static ArrayList<String[]> results = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        ArrayList<String[]> infos = new ArrayList<>();
         final int[] index = {0};
 
-        JFrame jFrame = new JFrame("小可爱");
+        JFrame jFrame = new JFrame("大可爱");
         Box box = Box.createVerticalBox();
-        
+
         JButton jBtn_1 = new JButton("复制1");
-        JTextField jTextField_1 = new JTextField("区域1");
+        jTextField_1 = new JTextField("区域1");
         jBtn_1.addActionListener(e -> {
             String text1 = jTextField_1.getText();
             setSysClipboardText(text1);
@@ -30,7 +33,7 @@ public class Main {
         box1.add(jBtn_1);
 
         JButton jBtn_2 = new JButton("复制2");
-        JTextField jTextField_2 = new JTextField("区域2");
+        jTextField_2 = new JTextField("区域2");
         jBtn_2.addActionListener(e -> {
             String text2 = jTextField_2.getText();
             setSysClipboardText(text2);
@@ -40,7 +43,7 @@ public class Main {
         box2.add(jBtn_2);
 
         JButton jBtn_3 = new JButton("复制3");
-        JTextField jTextField_3 = new JTextField("区域3");
+        jTextField_3 = new JTextField("区域3");
         jBtn_3.addActionListener(e -> {
             String text3 = jTextField_3.getText();
             setSysClipboardText(text3);
@@ -62,38 +65,124 @@ public class Main {
 
         Box box5 = Box.createHorizontalBox();
         JButton jBtn_5 = new JButton("格式化邮箱");
-        JButton jBtn_5_2 = new JButton("下一个");
-        JTextArea jTextArea_1 = new JTextArea("邮箱源格式");
+        JTextArea jTextArea_1 = new JTextArea();
         JScrollPane jScrollPane_1 = new JScrollPane();
         jScrollPane_1.setViewportView(jTextArea_1);
         jBtn_5.addActionListener((e) -> {
+            index[0] = 0;
+            infos.clear();
+            results.clear();
             String str = jTextArea_1.getText();
             String[] lines = str.split("\n");
             for ( String line : lines ){
                 String[] info = line.split("----");
                 infos.add(info);
-
             }
-        });
-        jBtn_5_2.addActionListener((e)->{
-            String[] info = infos.get(index[0]);
-            index[0]++;
-            jTextField_1.setText(info[2]);
-            jTextField_2.setText(info[0]);
-            jTextField_3.setText(info[1]);
-            jTextField_4.setText("2018@asdfg");
+            if(index[0] < infos.size()){
+                String[] info = infos.get(index[0]);
+                jTextField_1.setText("2018@asdfg");
+                jTextField_2.setText(info[2]);
+                jTextField_3.setText(info[3]);
+                index[0]++;
+            }else{
+                JOptionPane.showMessageDialog(jFrame,"吃一口屎冷静下","你的邮箱输完了",JOptionPane.INFORMATION_MESSAGE);
+            }
 
         });
         box5.add(jScrollPane_1);
         box5.add(jBtn_5);
-        box5.add(jBtn_5_2);
+//        box5.add(jBtn_5_2);
+
+        Box box6 = Box.createHorizontalBox();
+        jTextField_6_1 = new JTextField();
+        jTextField_6_1.addFocusListener(new JTextFieldHintListener("IP",jTextField_6_1));
+        jTextField_6_2 = new JTextField();
+        jTextField_6_2.addFocusListener(new JTextFieldHintListener("手机号",jTextField_6_2));
+        jTextField_6_3 = new JTextField();
+        jTextField_6_3.addFocusListener(new JTextFieldHintListener("房屋ID",jTextField_6_3));
+        box6.add(jTextField_6_1);
+        box6.add(jTextField_6_2);
+        box6.add(jTextField_6_3);
+
+        Box box7 = Box.createHorizontalBox();
+        JButton jButton_7 = new JButton("格式化所有信息并复制到剪切板");
+        jButton_7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StringBuilder sb;
+                StringBuilder sb_sum = new StringBuilder();
+                for(String[] result : results){
+                    sb = new StringBuilder();
+                    for (int i = 0; i < result.length; i++) {
+                        sb.append(result[i]);
+                        sb.append("\t");
+                    }
+                    sb_sum.append(sb.toString());
+                    sb_sum.append("\n");
+                }
+                setSysClipboardText(sb_sum.toString());
+                JOptionPane.showMessageDialog(jFrame,"提示","复制成功了宝",JOptionPane.INFORMATION_MESSAGE);
+
+            }
+        });
+        box7.add(jButton_7);
+
+        //国家选择,状态选择
+        Box box8 = Box.createHorizontalBox();
+        JButton jBtn_8_1 = new JButton("成功");
+        JButton jBtn_8_2 = new JButton("失败");
+        jComboBox = new JComboBox();
+        jComboBox.addItem("ph");
+        jComboBox.addItem("vn");
+        jComboBox.addItem("kh");
+        box8.add(jComboBox);
+        jBtn_8_1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                insertResultData(true);
+                jTextField_6_1.setText("");
+                jTextField_6_2.setText("");
+                jTextField_6_3.setText("");
+                if(index[0] < infos.size()){
+                    String[] info = infos.get(index[0]);
+                    jTextField_1.setText("2018@asdfg");
+                    jTextField_2.setText(info[2]);
+                    jTextField_3.setText(info[3]);
+                    index[0]++;
+                }else{
+                    JOptionPane.showMessageDialog(jFrame,"吃一口屎冷静下","你的邮箱输完了",JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+        jBtn_8_2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                insertResultData(false);
+                jTextField_6_1.setText("");
+                jTextField_6_2.setText("");
+                jTextField_6_3.setText("");
+                if(index[0] < infos.size()){
+                    String[] info = infos.get(index[0]);
+                    jTextField_1.setText("2018@asdfg");
+                    jTextField_2.setText(info[2]);
+                    jTextField_3.setText(info[3]);
+                    index[0]++;
+                }else{
+                    JOptionPane.showMessageDialog(jFrame,"吃一口屎冷静下","你的邮箱输完了",JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+        box8.add(jBtn_8_1);
+        box8.add(jBtn_8_2);
 
         box.add(box5);
         box.add(box1);
         box.add(box2);
         box.add(box3);
         box.add(box4);
-
+        box.add(box6);
+        box.add(box8);
+        box.add(box7);
 
         jFrame.add(box);
         jFrame.pack();
@@ -101,9 +190,27 @@ public class Main {
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    //复制信息到剪切板
     public static void setSysClipboardText(String writeMe) {
         Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
         Transferable tText = new StringSelection(writeMe);
         clip.setContents(tText, null);
     }
+
+    //插入结果数据
+    public static void insertResultData(boolean state){
+        String[] result = new String[10];
+        result[0] = jTextField_1.getText();     //固定密码
+        result[1] = " ";                        //账户
+        result[2] = " ";                        //密码
+        result[3] = jTextField_2.getText();     //邮箱账户
+        result[4] = jTextField_3.getText();     //邮箱密码
+        result[5] = jTextField_6_1.getText();   //IP
+        result[6] = (String) jComboBox.getSelectedItem();//国家简写
+        result[7] = jTextField_6_3.getText();   //房屋ID
+        result[8] = jTextField_6_2.getText();   //手机号
+        result[9] = state?"成功":"失败";
+        results.add(result);
+    }
+
 }
